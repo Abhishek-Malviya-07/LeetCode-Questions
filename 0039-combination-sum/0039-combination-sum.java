2f994dcaf81036f3f44e8dc691023e15ca7a1c3e
Abhishek Malviya <90751735+Abhishek-Malviya-07@users.noreply.github.com>
2023-07-28 10:25:39 +0530

    Time: 1 ms (100.00%), Space: 44 MB (68.32%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,34 +1,23 @@
+
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-       List<List<Integer>> ans = new ArrayList<List<Integer>>();
-       List<Integer> currAns = new ArrayList<Integer>();
-       int currSum =0;
-       int index =0;
-       
-       solve(candidates , target,ans , currAns , currSum , index);
-       return ans;
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> currAns = new ArrayList<>();
+        Arrays.sort(candidates); // Sort the candidates array to optimize pruning
+        solve(candidates, target, ans, currAns, 0);
+        return ans;
     }
-    
-    static void solve(int[] c , int target ,  List<List<Integer>> ans, List<Integer> currAns, int currSum,int                                                                                                           index){
-        
-        if(currSum > target){
+
+    static void solve(int[] c, int target, List<List<Integer>> ans, List<Integer> currAns, int start) {
+        if (target == 0) {
+            ans.add(new ArrayList<>(currAns));
             return;
         }
-        if(index == c.length){
-           if(currSum == target){
-            ans.add(new ArrayList<Integer>(currAns));
-               
-            }
-            return;
+
+        for (int i = start; i < c.length && c[i] <= target; i++) {
+            currAns.add(c[i]);
+            solve(c, target - c[i], ans, currAns, i); // Use i instead of index+1 to avoid duplicates
+            currAns.remove(currAns.size() - 1);
         }
-         
-        
-        currSum += c[index];
-        currAns.add(c[index]);
-        solve(c , target , ans,currAns, currSum,index);
-        currSum -= c[index];
-        currAns.remove(currAns.size()-1);
-        
-        solve(c , target , ans,currAns, currSum,index+1);
     }
 }
