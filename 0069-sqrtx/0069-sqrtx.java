039be07bdb84d0630a0216fd4add253fdd4ca087
Abhishek Malviya <90751735+Abhishek-Malviya-07@users.noreply.github.com>
2026-01-08 17:06:14 +0530

    Time: 1 ms (96.3%), Space: 42.8 MB (10.22%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,8 +1,22 @@
-import java.util.*;
 class Solution {
     public int mySqrt(int x) {
+
+        int s = 1;
+        int e = x;
         int ans = 0;
-        ans = (int)Math.sqrt(x);
+
+        while (s <= e) {
+            int mid = s + (e - s) / 2;
+
+            if (mid <= x / mid) {
+                ans = mid;
+                s = mid + 1;
+            } else {
+                e = mid - 1;
+            }
+
+        }
+
         return ans;
     }
 }
