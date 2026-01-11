c1e20a8484567758d310014135a980a04ded605d
Abhishek Malviya <90751735+Abhishek-Malviya-07@users.noreply.github.com>
2026-01-11 10:16:26 +0530

    Time: 16 ms (43.02%), Space: 50.4 MB (11.45%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.java
+++ 0085-maximal-rectangle/0085-maximal-rectangle.java
@@ -1,60 +1,48 @@
 class Solution {
     public int maximalRectangle(char[][] matrix) {
-        if(matrix.length == 0)  return 0;
+        if (matrix == null || matrix.length == 0) return 0;
+        
+        int cols = matrix[0].length;
+        int[] heights = new int[cols];
         int maxArea = 0;
-        int row = matrix.length;
-        int col = matrix[0].length;
-        int[] dp = new int[col];
-        for(int i=0;i<row;i++){
-            for(int j=0;j<col;j++){
-                dp[j] = matrix[i][j] == '1' ? dp[j]+1 : 0;
+        
+        for (char[] row : matrix) {
+           
+            for (int j = 0; j < cols; j++) {
+                if (row[j] == '1') {
+                    heights[j]++;
+                } else {
+                    heights[j] = 0;
+                }
             }
-            //treating dp[j] as histogram, solving max area problem there and updating the max area
-            maxArea = Math.max(maxArea, findMaxAreaInHistogram(dp));
+            
+            
+            maxArea = Math.max(maxArea, largestRectangleArea(heights));
         }
+        
         return maxArea;
     }
-	//84. Largest Rectangle in Histogram code
-    public int findMaxAreaInHistogram(int[] dp){
-        int len = dp.length;
-        int maxArea = 0;
-        int[] left = new int[len];
-        int[] right = new int[len];
+
+    private int largestRectangleArea(int[] heights) {
         Stack<Integer> stack = new Stack<>();
-        //traversing left to right, finding left limit
-        for(int i=0;i<len;i++){
-            if(stack.isEmpty()){
-                stack.push(i);
-                left[i] = 0;
-            }else{
-                while(!stack.isEmpty() && dp[stack.peek()] >= dp[i])
-                    stack.pop();
-                left[i] = stack.isEmpty() ? 0 : stack.peek()+1;
-                stack.push(i);
-            }
-        }
-        //doing empty to stack
-        while(!stack.isEmpty())
-            stack.pop();
+        stack.push(-1);
+        int maxArea = 0;
         
-        //traversing right to left, find right limit
-        for(int i=len-1;i>=0;i--){
-            if(stack.isEmpty()){
-                stack.push(len-1);
-                right[i] = len - 1;
-            }else{
-                while(!stack.isEmpty() && dp[stack.peek()] >= dp[i])
-                    stack.pop();
-                right[i] = stack.isEmpty() ? len-1 : stack.peek()-1;
-                stack.push(i);
+        for (int i = 0; i < heights.length; i++) {
+            while (stack.peek() != -1 && heights[stack.peek()] >= heights[i]) {
+                int height = heights[stack.pop()];
+                int width = i - stack.peek() - 1;
+                maxArea = Math.max(maxArea, height * width);
             }
+            stack.push(i);
         }
-        //traversing the array , caculating area
-        int[] area = new int[len];
-        for(int i=0;i<len;i++){
-            area[i] = (right[i] - left[i] + 1) * dp[i];
-            maxArea = Math.max(maxArea, area[i]);
+        
+        while (stack.peek() != -1) {
+            int height = heights[stack.pop()];
+            int width = heights.length - stack.peek() - 1;
+            maxArea = Math.max(maxArea, height * width);
         }
+        
         return maxArea;
     }
 }
