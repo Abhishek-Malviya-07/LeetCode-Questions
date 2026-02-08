7963c7f43ea3e2135555d6f016058cbdc81e07a3
Abhishek Malviya <90751735+Abhishek-Malviya-07@users.noreply.github.com>
2026-02-08 12:47:12 +0530

    Time: 1 ms (83.51%), Space: 44.3 MB (84.56%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,31 +1,29 @@
 class Solution {
-    
-     public void DFS(int n, int left, int right, List<String> result, StringBuilder sb) {
-        if (left == n && right == n) {
-            result.add(sb.toString());
-            return;
-        }
-        
-        if (left < n) {
-            sb.append('(');
-            DFS(n, left + 1, right, result, sb);
-            sb.deleteCharAt(sb.length() - 1);
-        }
-        if (right < left) {
-            sb.append(')');
-            DFS(n, left, right + 1, result, sb);
-            sb.deleteCharAt(sb.length() - 1);
-        }
-    }
-    
     public List<String> generateParenthesis(int n) {
-        List<String> result = new ArrayList<>();
-        if (n < 1) {
-            return result;
-        }
         
-        DFS(n, 0, 0, result, new StringBuilder());
-        
-        return result;
+        List<String> ans = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+        dfs(0,0,n,ans,sb);
+        return ans;
     }
+
+  static void dfs(int left , int right , int n , List<String> ans , StringBuilder sb){
+
+    if(left == n && right == n){
+        ans.add(sb.toString());
+        return;
+    }
+
+    if(left<n){
+        sb.append("(");
+        dfs(left+1 , right ,n , ans , sb);
+        sb.deleteCharAt(sb.length() - 1); 
+    }
+
+    if(right < left){
+        sb.append(")");
+        dfs(left , right+1 , n ,ans , sb);
+        sb.deleteCharAt(sb.length() - 1); 
+    }
+  }
 }
