f3e7c404a25f9a9bfe935016999e0ed38a770fb6
Abhishek Malviya <90751735+Abhishek-Malviya-07@users.noreply.github.com>
2026-04-10 21:41:38 +0530

    Time: 3 ms (82.64%), Space: 45.3 MB (14.47%) - LeetHub

---
+++ 3740-minimum-distance-between-three-equal-elements-i/3740-minimum-distance-between-three-equal-elements-i.java
@@ -0,0 +1,30 @@
+public class Solution {
+    public int minimumDistance(int[] nums) {
+        int n = nums.length;
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        
+        for (int i = 0; i < n; i++) {
+            map.putIfAbsent(nums[i], new ArrayList<>());
+            map.get(nums[i]).add(i);
+        }
+        
+        int minDistance = Integer.MAX_VALUE;
+        boolean found = false;
+        
+        for (List<Integer> indices : map.values()) {
+          
+            if (indices.size() >= 3) {
+                found = true;
+                
+                for (int i = 0; i <= indices.size() - 3; i++) {
+                    int first = indices.get(i);
+                    int last = indices.get(i + 2);
+                    int currentDistance = 2 * (last - first);
+                    minDistance = Math.min(minDistance, currentDistance);
+                }
+            }
+        }
+        
+        return found ? minDistance : -1;
+    }
+}
