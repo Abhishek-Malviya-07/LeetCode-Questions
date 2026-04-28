f7711b471723845a45f93eddfd41e93c32878b8d
Abhishek Malviya <90751735+Abhishek-Malviya-07@users.noreply.github.com>
2026-04-28 22:36:28 +0530

    Time: 31 ms (94.66%), Space: 110.7 MB (51.91%) - LeetHub

---
+++ 2033-minimum-operations-to-make-a-uni-value-grid/2033-minimum-operations-to-make-a-uni-value-grid.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int minOperations(int[][] grid, int x) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int[] arr = new int[m * n];
+        int index = 0;
+       
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                arr[index++] = grid[i][j];
+            }
+        }
+        
+      
+        int remainder = arr[0] % x;
+        for (int val : arr) {
+            if (val % x != remainder) {
+                return -1;
+            }
+        }
+        
+       
+        Arrays.sort(arr);
+        int median = arr[arr.length / 2];
+        
+       
+        int totalOps = 0;
+        for (int val : arr) {
+            totalOps += Math.abs(val - median) / x;
+        }
+        
+        return totalOps;
+    }
+}
